e402cc9321292128db2e37c68dc384d6c78d596a
Super <super.exists@gmail.com>
2026-09-17 22:53:17 +0300

    Remove Accessibility settings construction banner

--- App/Sources/SakuraCord/Views/Settings/SettingsPageComponents.swift
+++ App/Sources/SakuraCord/Views/Settings/SettingsPageComponents.swift
@@ -61,7 +61,7 @@ private extension SettingsPageID {
     var showsConstructionNotice: Bool {
         switch self {
         case .profiles, .interface, .appearance, .privacySafety, .storageDownloads, .diagnostics,
-             .softwareUpdates, .extensions, .about, .keyboardShortcuts:
+             .softwareUpdates, .extensions, .about, .keyboardShortcuts, .accessibility:
             false
         default:
             true
